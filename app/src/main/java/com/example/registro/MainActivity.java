8f3653eb1927865fdaf170bead7df1225f2d4991
Julia Gonzales <juliagonzaleszegarra@gmail.com>
2023-05-30 01:03:16 -0400

    individual 6 y 6.1

--- app/src/main/java/com/example/registro/MainActivity.java
+++ app/src/main/java/com/example/registro/MainActivity.java
@@ -12,6 +12,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText textNombre;
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     EditText textPassword;
 
     Button buttonRegistrarse;
+    String pass = "1234";
 
 
     @Override
@@ -27,16 +31,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textNombre = findViewById(R.id.textNombre);
-        textApellido = findViewById(R.id.textApellido);
-        textEmail = findViewById(R.id.textEmail);
-        textPassword = findViewById(R.id.textPassword);
+        textNombre = (EditText) findViewById(R.id.textNombre);
+        textApellido = (EditText) findViewById(R.id.textApellido);
+        textEmail = (EditText) findViewById(R.id.textEmail);
+        textPassword = (EditText) findViewById(R.id.textPassword);
 
-        buttonRegistrarse  = findViewById(R.id.buttonRegistrarse);
+        buttonRegistrarse =(Button) findViewById(R.id.buttonRegistrarse);
 
         buttonRegistrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //toast
+                Context context = getApplicationContext();
+                CharSequence text = "Datos vacios";
+                CharSequence text2 = "error de password";
+                CharSequence text3 = "solo se permiten letras, nombre y apellido no validos";
+                CharSequence text4 = "formato de correo no valido";
+                int duracion = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duracion);
+                Toast toast2 = Toast.makeText(context, text2, duracion);
+                Toast toast3 = Toast.makeText(context, text3, duracion);
+                Toast toast4 = Toast.makeText(context, text4, duracion);
+
 
                 // obtenemos los datos del usuario.
                 String nombre = textNombre.getText().toString();
@@ -44,21 +61,45 @@ public class MainActivity extends AppCompatActivity {
                 String correo = textEmail.getText().toString();
                 String clave = textPassword.getText().toString();
 
-                //creamos intent para iniciar la segunda actividad.
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                //validamos campos vacios
+                if ((!textNombre.getText().toString().trim().equals("")) && (!textApellido.getText().toString().trim().equals(""))
+                        && (!textEmail.getText().toString().trim().equals(""))) {
+                    //patrones de validacion
+                    Pattern pat = Pattern.compile("[a-zA-Z]{2,10}");
+                    Pattern patEmail = Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+                    Matcher mat = pat.matcher(textNombre.getText().toString());
+                    Matcher mat2 = pat.matcher(textApellido.getText().toString());
+                    Matcher mat3 = patEmail.matcher(textEmail.getText().toString());
+                    //validamos que nombre y apellido sean validos, letras mayusculas y minusculas
+                    if (mat.matches() && mat2.matches()) {
+                        //validamos formato de correo
+                        if (mat3.matches()) {
+                            //validamos contraseña
+                            if (textPassword.getText().toString().equals(pass)) {
+                                //creamos intent para iniciar la segunda actividad.
+                                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                                intent.putExtra("nombre", nombre);
+                                intent.putExtra("apellido", apellido);
+                                intent.putExtra("correo", correo);
+                                intent.putExtra("clave", clave);
+                                startActivity(intent);
 
-                intent.putExtra("nombre", nombre);
-                intent.putExtra("apellido", apellido);
-                intent.putExtra("correo", correo);
-                intent.putExtra("clave", clave);
-                startActivity(intent);
+                            } else {
+                                toast2.show();
+                            }
+                        } else {
+                            toast4.show();
+                        }
 
+                    } else {
+                        toast3.show();
+                    }
+
+                } else {toast.show();}
             }
-        });
-
+            });
+    }
 
     }
-}
-
 
 
